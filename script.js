eb2679c487dd3ed6230ac248ab775dd0e1c038aa
Prakash Kumar <141754712+prakash-ydv@users.noreply.github.com>
2024-03-05 15:00:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.getElementById('menu_bar').addEventListener('click', function() {
+document.getElementById('menu_bar').addEventListener('click', function () {
     document.querySelector('.dropMenu').classList.toggle('showDropMenu');
-  });
-  
+});
+
+const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000, // Animation speed in milliseconds
+    easing: 'easeInOutCubic', // Easing function
+    offset: 100, // Scroll offset in pixels
+});
+
+
+function toggleAnswer(id) {
+    // Hide all answers
+    document.querySelectorAll('.faq-answer').forEach(function(answer) {
+        answer.classList.remove('show');
+    });
+    // Show the answer for the clicked question
+    document.getElementById('answer' + id).classList.add('show');
+}
